94ab0e5b556f1d18c76c258aa4e829dbd139f1a2
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-30 10:24:07 +0530

    ADDED: Created tictactoe game board

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -1,7 +1,29 @@
 package com.tictactoe;
 
 public class TicTacToe {
+	char[] board = new char[10];
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
+
+		char[] board = new char[10];
+
+		createBoard(board, 10);
+		printBoard(board);
+	}
+
+	public static void createBoard(char[] board, int size) {
+		board[0] = 'D';
+		for (int i = 1; i < board.length; i++) {
+			board[i] = ' ';
+		}
+	}
+
+	public static void printBoard(char[] board) {
+		for (int i = 1; i < board.length; i++) {
+			System.out.print("values " + board[i] + " ");
+			if (i % 3 == 0)
+				System.out.println("\n");
+		}
 	}
 }
